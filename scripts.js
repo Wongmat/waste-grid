798532e8cc98feeecf01cb97d04cef50d344064d
Mat <matwong05@gmail.com>
2018-12-06 20:14:09 +0800

    Minor fixes

--- scripts.js
+++ scripts.js
@@ -19,33 +19,10 @@ function removeGrid(number){
     $("#reshow").css('display', 'block');
 };  
 
-// function that prompts the user to select the number of boxes in a new grid
-// the function then also creates that new grid
-function refreshGrid(){
-    var z = prompt("How many boxes per side?");
-    clearGrid();
-    createGrid(z);
-};
-
-// create a 16x16 grid when the page loads
-// creates a hover effect that changes the color of a square to black when the mouse passes over it, leaving a (pixel) trail through the grid
-// allows the click of a button to prompt the user to create a new grid
 $(document).ready(function() {
     createGrid(25);
     hkPop = 7337000;
     waste = 3776250;
-
-    $(".grid").mouseover(function() {
-        $(this).css("background-color", "black");
-        });
-
-    $(".newGrid").click(function() {
-        refreshGrid();
-
-        $(".grid").mouseover(function() {
-        $(this).css("background-color", "black");
-        });
-    });
 
     $("#overlayButton").click(function() {
         document.getElementById("overlay").style.display = "none";
@@ -67,7 +44,7 @@ $(document).ready(function() {
 
     $("#food").on('click', () => {
         removeGrid(109);
-        $('#headline').html('By far, the <b>food waste</b> is the type of garbage that makes up most landfill trash.');
+        $('#headline').html('By far, <b>food waste</b> is the type of garbage that makes up most landfill trash.');
         $('#middle').html('Food waste makes up a whopping <b>36%</b> of total landfill waste.');
         $('#bottom').html('Think before you eat!');
     });
